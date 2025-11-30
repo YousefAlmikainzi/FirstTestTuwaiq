e4412ae939f77759c704a99454e86f9fc52162cd
Yuzu1999 <63368986+YousefAlmikainzi@users.noreply.github.com>
2025-11-30 18:31:32 +0300

    player reset

---
+++ Assets/Scripts/ResetGame.cs
@@ -0,0 +1,24 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetGame : MonoBehaviour
+{
+    float deathFall;
+    [SerializeField] float fallenPlayer = 0;
+    void OnTriggerEnter(Collider other)
+    {
+        if ((other.gameObject.CompareTag("FireBall")))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+    void Update()
+    {
+        deathFall = transform.position.y;
+        if(deathFall <= fallenPlayer)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
